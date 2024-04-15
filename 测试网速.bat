51e9ff43ad917f8979ee62a29ab4deb0e67bc984
a <test@aa.com>
2024-04-15 21:24:36 +0800

    init

--- 测试网速.bat
+++
@@ -1,1 +0,0 @@
-python main.py --base_url 127.0.0.1:51654 --secret e69f998b-3e37-4d84-82d8-8a45afd556d3 --proxy_url 127.0.0.1:7890 --timeout 5 --max_size 10485760 --group_name "\ud83d\ude80\u0020\u8282\u70b9\u9009\u62e9"
